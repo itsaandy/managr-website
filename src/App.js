0285d52842f98f6db310e9249f2dfc30f88ff501
Aditya Pokharel <pokhareladitya@gmail.com>
2023-05-20 15:22:18 +1000

    add basic landing page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from "./logo.png"
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Coming soon</h1>
+        <img
+            className="logo"
+            src={logo} alt="logo"
+        />
+    </div>
+  );
+}
+
+export default App;
